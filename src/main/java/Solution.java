bb252f0406542a8fb92f98a70c81f09b1212371c
Owen Anderson <owenthomasanderson@gmail.com>
2018-03-22 01:15:29 -0400

    added comments

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -14,10 +14,22 @@ import java.util.HashMap;
 
     Cube[] cubes;
 
+    /**
+     * Creates a soltution object from a list of cubes
+     * 
+     * @param cubes - The list of cubes to create the solution with
+     */
     public Solution(Cube[] cubes){
         this.cubes = cubes;
     }
 
+    /**
+     * Takes a solution and a cube and creates a new solution with
+     * the new cube added to it
+     * 
+     * @param other - The solution to copy into this one
+     * @param c - The cube to add to the end of this one after added in those from other
+     */
     public Solution(Solution other, Cube c){
         if(c == null){
             throw new IllegalArgumentException("The cube passed in must not be null");
@@ -60,11 +72,20 @@ import java.util.HashMap;
         return true;
     }
 
+    /**
+     * Checks if the solution is still valid after a given
+     * cube has been added to it
+     * 
+     * @param next - The cube to be added
+     */
     public boolean isValid(Cube next){
         Solution temp = new Solution(this, next);
         return temp.isValid();
     }
 
+    /**
+     * Gets the string representation of the solution object
+     */
     public String toString(){
         String outp = "[";
         for(int i = 0; i < this.size(); i++){
