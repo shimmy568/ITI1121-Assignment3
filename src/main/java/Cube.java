b039293804c2838c5fbd12e0e7dc46994bcdcc7c
Owen Anderson <owenthomasanderson@gmail.com>
2018-03-22 00:44:34 -0400

    work

--- src/main/java/Cube.java
+++ src/main/java/Cube.java
@@ -16,6 +16,9 @@ public class Cube{
     public Color[] faces;
     public Color[] initState;
 
+    /**
+     * Create a cube from a list of colors
+     */
     public Cube(Color[] faces){
         this.faces = faces;
         initState = new Color[faces.length];
@@ -28,6 +31,11 @@ public class Cube{
         initStepsList();
     }
 
+    /**
+     * The constuctor to make a copy of the cube
+     * 
+     * @param toCopy - The cube to make this cube a deep copy of
+     */
     public Cube(Cube toCopy){
         faces = new Color[toCopy.faces.length];
         for(int i = 0; i < toCopy.faces.length; i++){
@@ -45,6 +53,10 @@ public class Cube{
         }
     }
 
+    /**
+     * Sets up the list of steps the cube needs to take
+     * to go through all 24 states
+     */
     private void initStepsList(){
         steps.add(0);
         steps.add(1);
@@ -72,55 +84,91 @@ public class Cube{
         steps.add(1);
     }
 
+    /**
+     * Sets the cube to it's inital state
+     */
     private void identity(){
         for(int i = 0; i < this.faces.length; i++){
             faces[i] = initState[i];
         }
     }
 
+    /**
+     * Gets the top face of the cube
+     */
     public Color getUp(){
         return faces[0];
     }
 
+    /**
+     * Gets the front face of the cube
+     */
     public Color getFront(){
         return faces[0];
     }
 
+    /**
+     * Gets the right face of the cube
+     */
     public Color getRight(){
         return faces[0];
     }
 
+    /**
+     * Gets the back face of the cube
+     */
     public Color getBack(){
         return faces[0];
     }
 
+    /**
+     * Gets the left face of the cube
+     */
     public Color getLeft(){
         return faces[0];
     }
 
+    /**
+     * Gets the bottom of the cube
+     */
     public Color getDown(){
         return faces[0];
     }
 
+    /**
+     * Rotates the cube so the left side faces front
+     */
     private void rotate(){
         faces = new Color[]{faces[0], faces[4], faces[1], faces[2], faces[3], faces[5]};
     }
 
+    /**
+     * Rolls the cube to the right
+     */
     private void rightRoll(){
         faces = new Color[]{faces[4], faces[1], faces[0], faces[3], faces[5], faces[2]};
     }
 
+    /**
+     * Rolls the cube to the left
+     */
     private void leftRoll(){
         rightRoll();
         rightRoll();
         rightRoll();
     }
 
+    /**
+     * Resets the cube to the inital state
+     */
     public void reset(){
         steps = new LinkedList<Integer>();
         initStepsList();
     }
 
+    /**
+     * Returns if the cube has any more unchecked states
+     */
     public boolean hasNext(){
         if(steps.size() > 0){
             return true;
@@ -129,6 +177,9 @@ public class Cube{
         }
     }
 
+    /**
+     * Moves the cube to the next state
+     */
     public void next(){
 
         if(steps.size() == 0){
@@ -152,6 +203,10 @@ public class Cube{
         steps.removeFirst();
     }
 
+    /**
+     * Converts this cube to it's string 
+     * representation
+     */
     public String toString(){
         String out = "[";
         for(int i = 0; i < this.faces.length; i++){
@@ -165,6 +220,9 @@ public class Cube{
         return out;
     }
 
+    /**
+     * Returns a deep copy of this cube
+     */
     public Cube copy(){
         return new Cube(this);
     }
--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+
 /**
  * This class represents a partial solutions to the problem.
  * 
@@ -8,3 +10,37 @@
  *
  */
 
+ public class Solution {
+
+    Cube[] cubes;
+
+    public Solution(Cube[] cubes){
+        this.cubes = cubes;
+    }
+
+    /**
+     * Returns the number of cubes in the solution
+     */
+    public int size(){
+        return this.cubes.length;
+    }
+
+    /**
+     * Checks if the solution is valid
+     */
+    public boolean isValid(){
+        for(int i = 0; i < 6; i++){
+            HashMap<Color, Boolean> foundColors = new HashMap<Color, Boolean>();
+            for(int o = 0; o < this.size(); o++){
+                if(foundColors.containsKey(this.cubes[o].faces[i])){
+                    return false;
+                }else{
+                    foundColors.put(this.cubes[o].faces[i], true);
+                }
+            }
+        }
+        return true;
+    }
+
+
+ }
